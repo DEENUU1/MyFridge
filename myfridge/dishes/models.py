f04d81c0e6a76cb3f763a696e34f9ad5d93c0887
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-02 01:26:01 +0200

    Feature | Model DifficultyLevel |
    Model which represents level of difficulty to make dish

--- myfridge/dishes/models.py
+++ myfridge/dishes/models.py
@@ -29,3 +29,11 @@ class OtherIngredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class DifficultyLevel(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
