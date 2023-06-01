56caec8cdc34db1b577e285e349373b25a9dbe03
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-02 01:19:37 +0200

    Feature | Model Country |
    Model to represent countries: Poland, USA, Italy etc

--- myfridge/dishes/models.py
+++ myfridge/dishes/models.py
@@ -7,3 +7,9 @@ class Type(models.Model):
     def __str__(self):
         return self.name
 
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
