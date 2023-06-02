fc097a1b49ce264bc1c0d9b8476e1f3c78ff0ccc
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-02 23:45:08 +0200

    Feature | Statute page and button to accept that statute |
    This view and button are for registration process
    To complete the registration form user need to accept that

--- myfridge/users/urls.py
+++ myfridge/users/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     ),
     path("login/", views.LoginUserView.as_view(), name="login"),
     path("logout/", views.LogoutUserView.as_view(), name="logout"),
+    path("statute/", views.StatuteView.as_view(), name="statute"),
 ]
