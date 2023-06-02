218ded8f388da59c8d58028041415053923a539d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-02 20:37:13 +0200

    Test | CustomUser model test |
    Test for CustomUser model to check if user is created successfully and has all fields

---
+++ myfridge/tests/test_users/test_models.py
@@ -0,0 +1,16 @@
+import pytest
+from users.models import CustomUser
+
+
+@pytest.mark.django_db
+def test_model_custom_user_successfully_created():
+    user = CustomUser.objects.create_user(
+        username="test user",
+        email="test@example.com",
+        password="Str0ngPassword1",
+        description="Simple test description",
+    )
+    assert user.username == "test user"
+    assert user.email == "test@example.com"
+    assert user.description == "Simple test description"
+    assert user.points == 0
