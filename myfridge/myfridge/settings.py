8d23d485c4033db14b1356c5d4cbe2a7fd8962fa
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-02 16:50:27 +0200

    Feature | Registration form |
    This form allows user to register has fields: username, email, password and repeated password
    
    Also has 3 methods (send_email) not implemented yet
    clean_email and clean_password_repeat allows to check if password already exists in database and does 2 passwords are the same

--- myfridge/myfridge/settings.py
+++ myfridge/myfridge/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'dishes',
+    'users',
+
 ]
 
 MIDDLEWARE = [
@@ -122,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
