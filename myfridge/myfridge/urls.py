522f4c35d08c58658ae0c0e84534c007b9655dca
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-02 17:43:13 +0200

    Feature | CustomUser model registration and forms library

--- myfridge/myfridge/urls.py
+++ myfridge/myfridge/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('user/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myfridge/users/admin.py
+++ myfridge/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "email",
+        "points",
+        "is_active",
+    ]
+
+    list_filter = ["is_active", "points"]
