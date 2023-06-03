73bb086a27c372b6c017e69c5c7118e6fc3f0890
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-03 02:18:43 +0200

    Feature | Home view, image field and admin site |
    Home view inherits from ListView and allows to display list of Dishes
    Pagination is set on 10 items per page
    
    Registered all models using admin decorator to make a custom admin page and display only the most important information and add custom fileters in admin page
    
    Added image field to Dish

--- myfridge/dishes/views.py
+++ myfridge/dishes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from typing import Any, Dict
 
-# Create your views here.
+from .models import Dish
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
+
+class HomeView(ListView):
+    model = Dish
+    template_name = "home.html"
+    paginated_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        return queryset
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        return context
