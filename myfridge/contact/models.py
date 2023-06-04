33f0793da130c181e703c37f29b93c690bf2dce8
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-04 12:59:13 +0200

    Feature | Initial contact app and models |
    This app allows comunication between website owner and user

---
+++ myfridge/contact/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class ContactSubject(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.ForeignKey(ContactSubject, on_delete=models.CASCADE)
+    message = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.username
--- myfridge/myfridge/settings.py
+++ myfridge/myfridge/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "dishes",
     "users",
     "social",
+    "contact",
 ]
 
 MIDDLEWARE = [
