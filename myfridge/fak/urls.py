84cc67400c5e299410351fd6ccc80eee9e855dcc
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-06 16:54:43 +0200

    Feature | Medicine CRUD operations |
    MedicineCreateView allows to create a new medicine for log in users
    MedicineUpdateView allows to edit existing medicine for log in users
    MedicineDeleteView allows to delete existing medicine for log in users

--- myfridge/fak/urls.py
+++ myfridge/fak/urls.py
@@ -9,4 +9,15 @@ urlpatterns = [
     path("create/", views.FakCreateView.as_view(), name="fak_create"),
     path("update/<int:pk>/", views.FakUpdateView.as_view(), name="fak_update"),
     path("delete/<int:pk>/", views.FakDeleteView.as_view(), name="fak_delete"),
+    path("medicine/create", views.MedicineCreateView.as_view(), name="medicine_create"),
+    path(
+        "medicine/update/<int:pk>/",
+        views.MedicineUpdateView.as_view(),
+        name="medicine_update",
+    ),
+    path(
+        "medicine/delete/<int:pk>/",
+        views.MedicineDeleteView.as_view(),
+        name="medicine_delete",
+    ),
 ]
--- myfridge/fak/views.py
+++ myfridge/fak/views.py
@@ -71,3 +71,45 @@ class FakDeleteView(LoginRequiredMixin, DeleteView):
         if not queryset.exists():
             raise PermissionDenied("You are not authorized to edit this Fak.")
         return queryset
+
+
+class MedicineCreateView(LoginRequiredMixin, CreateView):
+    model = Medicine
+    template_name = "medicine_create.html"
+    fields = ("name", "expiration_date", "quantity", "fak")
+    success_url = reverse_lazy("fak:fak_home")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        return context
+
+
+class MedicineUpdateView(LoginRequiredMixin, UpdateView):
+    model = Fak
+    template_name = "medicine_update.html"
+    fields = ("name", "expiration_date", "quantity", "fak")
+    success_url = reverse_lazy("fak:fak_home")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(author=self.request.user)
+        if not queryset.exists():
+            raise PermissionDenied("You are not authorized to edit this Medicine.")
+        return queryset
+
+
+class MedicineDeleteView(LoginRequiredMixin, DeleteView):
+    model = Fak
+    success_url = reverse_lazy("fak:fak_home")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(author=self.request.user)
+        if not queryset.exists():
+            raise PermissionDenied("You are not authorized to delete this Medicine.")
+        return queryset
