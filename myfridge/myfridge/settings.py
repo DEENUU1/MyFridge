072fffcff7a895e2a803e355523a4266f9a0a933
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-07 23:54:57 +0200

    Bugfix | Fixed settings.py file

--- myfridge/myfridge/settings.py
+++ myfridge/myfridge/settings.py
@@ -101,8 +101,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "myfridge.wsgi.application"
 
-Database
-https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
