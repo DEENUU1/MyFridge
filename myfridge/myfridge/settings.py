aebb4cb6f1a578a19b6bd1b237515021978de5ca
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-07 21:04:41 +0200

    Feature | Email and celery configuration

--- myfridge/myfridge/settings.py
+++ myfridge/myfridge/settings.py
@@ -66,6 +66,21 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+
+
 ROOT_URLCONF = "myfridge.urls"
 
 TEMPLATES = [
