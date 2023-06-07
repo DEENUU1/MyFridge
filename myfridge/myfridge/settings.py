294a6cc75f366f2f98667be28dc0efc4e4674fca
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-07 23:52:30 +0200

    Feature | Docker and docker compose and github action update

--- myfridge/myfridge/settings.py
+++ myfridge/myfridge/settings.py
@@ -104,10 +104,22 @@ WSGI_APPLICATION = "myfridge.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myfridge',
+        'USER': 'myfridge',
+        'PASSWORD': 'myfridge123',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
