91df036d2225950aff208cbff20056fd2588840e
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-10 11:18:29 +0200

    Feature | Celery beat configuration

--- myfridge/contact/task.py
+++ myfridge/contact/task.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from .models import Contact
 
 
-@shared_task
+@shared_task()
 def delete_old_contacts():
     time_threshold = timezone.now() - timedelta(days=60)
     Contact.objects.filter(date_created__lte=time_threshold).delete()
--- myfridge/myfridge/settings.py
+++ myfridge/myfridge/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "crispy_forms",
     "crispy_bootstrap5",
+    "django_celery_beat",
     "ckeditor",
     "dishes",  # project
     "users",  # project
