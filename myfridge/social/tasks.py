8266cb2bc35f9d09531187d117a9ad7a8d5ed010
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-11 15:28:05 +0200

    Feature | Function to return all dishes from database

--- myfridge/social/tasks.py
+++ myfridge/social/tasks.py
@@ -11,3 +11,8 @@ def get_users_with_newsletter_true():
     return CustomUser.objects.filter(newsletter=True)
 
 
+def get_random_dish():
+    dishes = Dish.objects.all()
+    random_dish = random.choice(dishes)
+    return random_dish
+
