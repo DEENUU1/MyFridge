5fe0d29ce9dd590e4865646a415299462ba96205
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-12 22:26:08 +0200

    Feature | 2 database setup sqlite for github action and postgresql for production/develop

--- myfridge/myfridge/settings.py
+++ myfridge/myfridge/settings.py
@@ -107,24 +107,26 @@ WSGI_APPLICATION = "myfridge.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+USE_SQLITE = os.getenv("USE_SQLITE")
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myfridge',
-        'USER': 'myfridge',
-        'PASSWORD': 'myfridge123',
-        'HOST': 'db',
-        'PORT': '5432',
+if USE_SQLITE:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'myfridge',
+            'USER': 'myfridge',
+            'PASSWORD': 'myfridge123',
+            'HOST': 'db',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
