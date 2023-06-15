f278ca9805298af2c9cacde173ddb5b520a52bbe
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-15 18:45:15 +0200

    Add url for RandomDishView #81

--- myfridge/dishes/urls.py
+++ myfridge/dishes/urls.py
@@ -7,6 +7,7 @@ app_name = "dishes"
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("dish/<int:pk>/", views.DishDetailView.as_view(), name="dish-detail"),
+    path("dish/random/", views.RandomDishView.as_view(), name="random-dish"),
     path("dish/create/", views.DishCreateView.as_view(), name="dish-create"),
     path("dish/<int:pk>/delete", views.DeleteDishView.as_view(), name="dish-delete"),
     path("dish/<int:pk>/update", views.UpdateDishView.as_view(), name="dish-update"),
