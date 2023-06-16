3202c579b494d73df9240a052b40170d37823c50
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-16 23:39:26 +0200

    Add model MealDailyPlan #85

--- myfridge/tools/models.py
+++ myfridge/tools/models.py
@@ -23,3 +23,35 @@ class Meal(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class MealDailyPlan(models.Model):
+    MONTHS = ((1, "January"),
+              (2, "February"),
+              (3, "March"),
+              (4, "April"),
+              (5, "May"),
+              (6, "June"),
+              (7, "July"),
+              (8, "August"),
+              (9, "September"),
+              (10, "October"),
+              (11, "November"),
+              (12, "December"))
+
+    YEARS = ((4,  "2023"),
+             (5,  "2024"),
+             (6,  "2025"),)
+
+    date = models.DateField()
+    month = models.IntegerChoices(MONTHS)
+    year = models.IntegerField(YEARS)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    breakfast = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='breakfast', null=True)
+    second_breakfast = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='second_breakfact', null=True)
+    lunch = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="lunch", null=True)
+    tea = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="tea", null=True)
+    dinner = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="dinner", null=True)
+
+    def __str__(self):
+        return f"{self.date} {self.month} {self.year}"
