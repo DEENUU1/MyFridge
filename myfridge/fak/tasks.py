1d971b27ab7b0054f9c7b43af4d37c8a97373cec
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-16 22:16:18 +0200

    Add function to send email notify about expired medicine #83

---
+++ myfridge/fak/tasks.py
@@ -0,0 +1,21 @@
+from celery import shared_task
+from users.models import CustomUser
+from .models import Medicine, Fak
+import datetime
+from users.task import send_email_task
+
+
+@shared_task()
+def send_medicine_expired_notification():
+    expired_medicines = Medicine.objects.filter(expiration_date__lt=datetime.datetime.now())
+    SUBJECT = "Your medicine is expired"
+    MESSAGE = f"Your medicine {expired_medicines.name} is expired"
+
+    for medicine in expired_medicines:
+        send_email_task(
+            [medicine.user.email],
+            SUBJECT,
+            MESSAGE,
+        )
+
+
