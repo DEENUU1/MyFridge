80ca86e38d704e4bff9f21550a530fadd3b9bbe4
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-16 22:32:29 +0200

    Add celery task to upload user profile image

--- myfridge/users/task.py
+++ myfridge/users/task.py
@@ -1,6 +1,9 @@
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.files.base import ContentFile
+from .models import CustomUser
+import requests
 
 
 @shared_task()
@@ -8,3 +11,9 @@ def send_email_task(email, subject, message):
     send_mail(subject, message, settings.EMAIL_HOST_USER, [email])
 
 
+@shared_task()
+def add_profile_picture(user_id, image_url):
+    user = CustomUser.objects.get(user_id=user_id)
+    response = requests.get(image_url)
+    image_name = image_url.split("/")[-1]
+    user.image.save(image_name, ContentFile(response.content), save=True)
