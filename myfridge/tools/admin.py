19b3005dc42fd3d72755fca4091ded6617e1642a
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-16 23:50:37 +0200

    Bugfix | Update models to avoid errors #85

--- myfridge/tools/admin.py
+++ myfridge/tools/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ShoppingList
+from .models import ShoppingList, Meal, MealDailyPlan
 
 
 @admin.register(ShoppingList)
@@ -7,3 +7,15 @@ class RateAdmin(admin.ModelAdmin):
     list_display = ["name", "date_created", "author", "quantity", "is_bought"]
 
     list_filter = ["date_created", "is_bought", "author"]
+
+
+@admin.register(Meal)
+class MealModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "user"]
+    list_filter = ["user"]
+
+
+@admin.register(MealDailyPlan)
+class MealDailyPlanModelAdmin(admin.ModelAdmin):
+    list_display = ["date", "month", "year", "user"]
+    list_filter = ["date", "month", "year", "user"]
--- myfridge/tools/models.py
+++ myfridge/tools/models.py
@@ -16,42 +16,42 @@ class ShoppingList(models.Model):
 
 class Meal(models.Model):
     name = models.CharField(max_length=200)
-    content = models.TextField(null=True)
-    url = models.URLField(null=True)
+    content = models.TextField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    dish = models.ForeignKey(Dish, on_delete=models.CASCADE, null=True)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class MealDailyPlan(models.Model):
-    MONTHS = ((1, "January"),
-              (2, "February"),
-              (3, "March"),
-              (4, "April"),
-              (5, "May"),
-              (6, "June"),
-              (7, "July"),
-              (8, "August"),
-              (9, "September"),
-              (10, "October"),
-              (11, "November"),
-              (12, "December"))
+    MONTHS = (("January", "January"),
+              ("February", "February"),
+              ("March", "March"),
+              ("April", "April"),
+              ("May", "May"),
+              ("June", "June"),
+              ("July", "July"),
+              ("August", "August"),
+              ("September", "September"),
+              ("October", "October"),
+              ("November", "November"),
+              ("December", "December"),)
 
-    YEARS = ((4,  "2023"),
-             (5,  "2024"),
-             (6,  "2025"),)
+    YEARS = (("2023",  "2023"),
+             ("2024",  "2024"),
+             ("2025",  "2025"),)
 
     date = models.DateField()
-    month = models.IntegerChoices(MONTHS)
-    year = models.IntegerField(YEARS)
+    month = models.CharField(max_length=20, choices=MONTHS)
+    year = models.CharField(max_length=20, choices=YEARS)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    breakfast = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='breakfast', null=True)
-    second_breakfast = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='second_breakfact', null=True)
-    lunch = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="lunch", null=True)
-    tea = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="tea", null=True)
-    dinner = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="dinner", null=True)
+    breakfast = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='breakfast', null=True, blank=True)
+    second_breakfast = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='second_breakfact', null=True, blank=True)
+    lunch = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="lunch", null=True, blank=True)
+    tea = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="tea", null=True, blank=True)
+    dinner = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="dinner", null=True, blank=True)
 
     def __str__(self):
         return f"{self.date} {self.month} {self.year}"
