470f8e9ddabcfb9e2ae5ce7458c36d0fab68b527
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-17 15:56:59 +0200

    Feature | Add model Comment #98

--- myfridge/users/models.py
+++ myfridge/users/models.py
@@ -17,3 +17,13 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} {self.post}"
