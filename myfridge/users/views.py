62e3375dd978e123e8cbb9f1eeaf23e5db5cce1a
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-17 20:42:59 +0200

    Feature | Add view to display a list of Posts #98

--- myfridge/users/views.py
+++ myfridge/users/views.py
@@ -19,6 +19,7 @@ from django.views.generic import (
     UpdateView,
     ListView,
     DetailView,
+    TemplateView
 )
 from django.views.generic.edit import FormView
 from dotenv import load_dotenv
@@ -246,4 +247,15 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
         queryset = queryset.filter(author=self.request.user)
         if not queryset.exists():
             raise PermissionDenied("You are not authorized to edit this Shopping List.")
+        return queryset
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(author=self.request.user)
         return queryset
