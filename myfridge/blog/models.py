2e7336a9721c8912f5baa4d3d2f23208daad4422
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-18 17:34:58 +0200

    Feature | Add RichTextField for text field in Post model #110

--- myfridge/blog/models.py
+++ myfridge/blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from users.models import CustomUser
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = RichTextField(blank=True, null=True)
     image = models.ImageField(upload_to="images", default=None, blank=True, null=True)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
