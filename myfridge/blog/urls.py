b5d3d778205240cc234b4a72a1a64fb59fff654c
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-18 19:05:52 +0200

    Bugfix | Update pytest for blog views #110

--- myfridge/blog/urls.py
+++ myfridge/blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("post/create/", views.PostCreateView.as_view(), name="create_post"),
     path("post/update/<int:pk>/", views.PostUpdateView.as_view(), name="update_post"),
     path("post/delete/<int:pk>/", views.PostDeleteView.as_view(), name="delete_post"),
-    path("post/", views.PostListView.as_view(), name="post_list"),
+    path("", views.PostListView.as_view(), name="post_list"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
     path("comment/create/<int:post_id>/", views.CommentCreateView.as_view(), name="create_comment"),
     path(
