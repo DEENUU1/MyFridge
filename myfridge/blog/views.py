3199067cb9fcfcda53ed7c3b9351938b14d24a7e
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-19 19:03:08 +0200

    Feature | Add user 1 point after add comment #114

--- myfridge/blog/views.py
+++ myfridge/blog/views.py
@@ -91,6 +91,11 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         form.instance.post = get_object_or_404(Post, pk=self.kwargs["post_id"])
+
+        user = self.request.user
+        user.points += 1
+        user.save()
+
         return super().form_valid(form)
 
     def get_success_url(self):
