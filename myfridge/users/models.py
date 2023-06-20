a18d64420b134e9ffa6a098d75bb39766016f410
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-20 12:55:01 +0200

    Feature | Add notification for a new follower #142

--- myfridge/users/models.py
+++ myfridge/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from notifications.signals import notify
 
 
 class CustomUser(AbstractUser):
@@ -17,3 +19,13 @@ class UserFollowing(models.Model):
 
     def __str__(self):
         return f'{self.user_id} is following {self.following_user_id}'
+
+
+def notification_new_follower(sender, instance, created, **kwargs):
+    if created:
+        notify.send(
+            instance.following_user_id,
+            recipient=instance.following_user_id,
+            verb=f"{instance.user_id} is now following you!",
+            target=instance.UserFollowing
+            )
