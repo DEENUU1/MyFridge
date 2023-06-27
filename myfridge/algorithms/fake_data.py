6901fe64c80dd79d3cda05b5022e03c7d9fa40e5
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-27 21:04:24 +0200

    Add script to create fake data for Meal, DailyMealPlan and ShoppingList models (#188)

--- myfridge/algorithms/fake_data.py
+++ myfridge/algorithms/fake_data.py
@@ -1,5 +1,6 @@
 from faker import factory, Faker
 from users.models import CustomUser, UserFollowing
+from tools.models import ShoppingList, Meal, MealDailyPlan
 
 fake = Faker()
 
@@ -38,3 +39,72 @@ def create_fake_user_data():
                 following_list.append(following)
 
             UserFollowing.objects.bulk_create(following_list)
+
+
+def create_fake_shopping_list():
+    num_shopping_lists = ShoppingList.objects.count()
+    new_shopping_lists = 50 - num_shopping_lists
+    shopping_list_list = []
+
+    if new_shopping_lists > 0:
+        for _ in range(new_shopping_lists):
+            shopping_list = ShoppingList(
+                name=fake.word(),
+                author=fake.random_element(elements=CustomUser.objects.all()),
+                quantity=fake.random_int(min=1, max=10),
+                is_bought=fake.boolean(),
+            )
+            shopping_list_list.append(shopping_list)
+
+        ShoppingList.objects.bulk_create(shopping_list_list)
+
+
+def create_fake_meal():
+    num_meals = Meal.objects.count()
+    new_meals = 50 - num_meals
+    meal_list = []
+    num_daily_plan = MealDailyPlan.objects.count()
+    new_daily_plan = 50 - num_daily_plan
+    meal_daily_plan_list = []
+    users = list(CustomUser.objects.all())
+    meals = list(Meal.objects.all())
+
+    if new_meals > 0:
+        for _ in range(new_meals):
+            meal = Meal(
+                name=fake.word(),
+                content=fake.text(max_nb_chars=200),
+                user=fake.random_element(elements=CustomUser.objects.all()),
+                url=fake.image_url(),
+            )
+            meal_list.append(meal)
+
+        Meal.objects.bulk_create(meal_list)
+
+        for _ in range(new_daily_plan):
+            date = fake.date_between(start_date="-1y", end_date="today")
+            month = date.strftime("%B")
+            year = str(date.year)
+            user = fake.random_element(users)
+            breakfast = fake.random_element(meals)
+            second_breakfast = fake.random_element(meals)
+            lunch = fake.random_element(meals)
+            tea = fake.random_element(meals)
+            dinner = fake.random_element(meals)
+            is_public = fake.boolean()
+
+            meal_plan = MealDailyPlan(
+                date=date,
+                month=month,
+                year=year,
+                user=user,
+                breakfast=breakfast,
+                second_breakfast=second_breakfast,
+                lunch=lunch,
+                tea=tea,
+                dinner=dinner,
+                is_public=is_public,
+            )
+
+            meal_daily_plan_list.append(meal_plan)
+        MealDailyPlan.bulk_create(meal_daily_plan_list)
